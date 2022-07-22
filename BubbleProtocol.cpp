a48cf41bc2968919d2042120d3ee9129e6e0e47c
maxwell <caiyunxiang@sjtu.edu.cn>
2022-07-22 11:30:36 +0800

    modify

--- BubbleProtocol.cpp
+++ BubbleProtocol.cpp
@@ -41,7 +41,7 @@ using namespace std;
 
 
 void bubble(struct Duallist *ALL_Vehicles, int slot){
-    struct Item * aItem, *bItem, *cItem;
+    struct Item * aItem;
     struct vehicle* aCar;
 
     if(log_process_flag == true){
@@ -54,16 +54,17 @@ void bubble(struct Duallist *ALL_Vehicles, int slot){
         aCar = (struct vehicle*)aItem->datap;               //[a,b] (rand()%(b-a+1))+a
 
 
-        if(strcmp(aCar->id, "a02.0") == 0){
-            if(aCar->packets->size()!=0){
-                cout<<"-----------------a02.0's packet number:"<<aCar->packets->size()<<endl;
-                for(auto aPkt: *(aCar->packets))
-                    cout<<"slot="<<slot<<"timestamp: "<<aPkt->timestamp<<"  src_vehicle:"<<aPkt->srcVehicle->id<<"  condition:"<<aPkt->condition<<endl;
-                }
-        }
+//        if(strcmp(aCar->id, "a02.0") == 0){
+//            if(aCar->packets->size()!=0){
+//                cout<<"-----------------a02.0's packet number:"<<aCar->packets->size()<<endl;
+//                for(auto aPkt: *(aCar->packets))
+//                    cout<<"slot="<<slot<<"timestamp: "<<aPkt->timestamp<<"  src_vehicle:"<<aPkt->srcVehicle->id<<"  condition:"<<aPkt->condition<<endl;
+//                }
+//        }
 
 
         handle_packets(aCar,slot);   //处理一帧以内收到的包的情况，更新进行决策的参考量;也要更新bubble_flag，看其是否被认可,等于true表明被认可，等于false表明不被认可
+
 
         if(log_process_flag == true){
             logACar(aCar);
@@ -72,7 +73,7 @@ void bubble(struct Duallist *ALL_Vehicles, int slot){
 
         if(aCar->slot_condition == SINGLE){
             if(slot < aCar->single_timestamp + SlotPerFrame){ //对于刚刚出现的车，需要听满一个frame才进行决策
-                // choose_commRange(aCar,slot); //决策通信半径,在一帧的中间要不要调整半径
+                choose_commRange(aCar,slot);
                 aItem = aItem->next;
                 continue;
             }else if(slot >= aCar->single_timestamp + SlotPerFrame){
@@ -257,37 +258,76 @@ int choose_slot(struct vehicle* aCar, int role, int slot){
     return choosed_slot;
 }
 
+
+
+
 //处理一辆车在过去一帧收到的packets
 void handle_packets(struct vehicle* aCar, int slot){
     struct Item *bItem;
-    //初始化记录的变量，也就是更新这些东西啦  创建车辆的时候把这些vector都要初始化好
-    //需要更新一帧以内的哪些信息呢？
-    //前车，后车，以及他们所对应的时槽使用情况
-    (*(aCar->front_Vehicles)).clear();
-    (*(aCar->rearV_Vehicles)).clear();
-    (*(aCar->queue_Vehicles)).clear();
-//    (*(aCar->queue_Vehicles_slot)).clear();
 
-    aCar->frontV = nullptr;
-    aCar->rearV = nullptr;
 
-    for(int ii = 0; ii < SlotPerFrame; ii++){
-        aCar->prev_OHN[ii] = aCar->OHN[ii];
-        aCar->OHN[ii] = nullptr;
-        aCar->THN[ii] = nullptr;
+    //在何时初始化这些统计量
+    if(aCar->role_condition == SINGLE & (slot - aCar->single_timestamp)%SlotPerFrame == 0){
+        aCar->frontV = nullptr;
+        aCar->rearV = nullptr;
+
+        for(int ii = 0; ii < SlotPerFrame; ii++){
+            aCar->prev_OHN[ii] = aCar->OHN[ii];
+            aCar->OHN[ii] = nullptr;
+            aCar->THN[ii] = nullptr;
+        }
+
+        //前车，后车，以及他们所对应的时槽使用情况
+        (*(aCar->front_Vehicles)).clear();
+        (*(aCar->rearV_Vehicles)).clear();
+        (*(aCar->queue_Vehicles)).clear();
+
+    }else if(aCar->role_condition == ACCESS && (slot - aCar->acceess_timestamp)%SlotPerFrame == 0){
+        aCar->frontV = nullptr;
+        aCar->rearV = nullptr;
+
+        for(int ii = 0; ii < SlotPerFrame; ii++){
+            aCar->prev_OHN[ii] = aCar->OHN[ii];
+            aCar->OHN[ii] = nullptr;
+            aCar->THN[ii] = nullptr;
+        }
+
+        //前车，后车，以及他们所对应的时槽使用情况
+        (*(aCar->front_Vehicles)).clear();
+        (*(aCar->rearV_Vehicles)).clear();
+        (*(aCar->queue_Vehicles)).clear();
+
+    }else if(aCar->role_condition == OCCUPIED){
+        aCar->frontV = nullptr;
+        aCar->rearV = nullptr;
+
+        for(int ii = 0; ii < SlotPerFrame; ii++){
+            aCar->prev_OHN[ii] = aCar->OHN[ii];
+            aCar->OHN[ii] = nullptr;
+            aCar->THN[ii] = nullptr;
+        }
+
+        //前车，后车，以及他们所对应的时槽使用情况
+        (*(aCar->front_Vehicles)).clear();
+        (*(aCar->rearV_Vehicles)).clear();
+        (*(aCar->queue_Vehicles)).clear();
+    }else{
+        cerr<<"Strange Role!!"<<endl;
     }
+
 
     int len = (*(aCar->packets)).size();
     for( int ii = len-1; ii>=0; ii-- ){
         struct packet* pkt = (struct packet*)(*(aCar->packets))[ii];
 
-        if(pkt->timestamp < slot - SlotPerFrame){//一帧之外的包就不看了
+        if(pkt->timestamp != slot){//一帧之外的包就不看了
             return;
         }
 
         if(pkt->condition == TX_COLI){//这个包属于两个同时发射，无法被感知到
            continue;
         }else if(pkt->condition == NO_COLI){//这种属于能够正常解的包
+
             //更新OHN
             int index = (pkt->timestamp)%SlotPerFrame;
             aCar->OHN[index] = pkt->srcVehicle;
@@ -308,8 +348,15 @@ void handle_packets(struct vehicle* aCar, int slot){
                 }
 
                 // 更新同车道前后最近邻车辆的信息
-                aCar->rearV = nearestVehicle(aCar, *(aCar->rearV_Vehicles));
-                aCar->frontV = nearestVehicle(aCar, *(aCar->front_Vehicles));
+
+                struct vehicle* tmp_rear = nearestVehicle(aCar, *(aCar->rearV_Vehicles));
+                struct vehicle* tmp_front = nearestVehicle(aCar, *(aCar->front_Vehicles));
+
+
+                if(tmp_rear != nullptr)
+                    aCar->rearV = tmp_rear;
+                if(tmp_front != nullptr)
+                    aCar->frontV = tmp_front;
 
                 // 更新车队信息,哪些车用了什么时槽
 //                    for(auto tmp: pkt->hashtable){
@@ -317,13 +364,8 @@ void handle_packets(struct vehicle* aCar, int slot){
 //                        int slot_index = tmp.second;
 //                        aCar->queue_Vehicles[bCar] = slot_index;
 //                    }
-                for(int ii = 0; ii < (*(pkt->hashtable_slot)).size(); ii++){
-                    struct vehicle* bCar = (*(pkt->hashtable_vehicles))[ii];
-                    int slot_index = (*(pkt->hashtable_slot))[ii];
-
-                    (*(aCar->queue_Vehicles)).push_back(bCar);
-//                    (*(aCar->queue_Vehicles_slot)).push_back(slot_index);
-                }
+                for(int ii = 0; ii < (*(pkt->hashtable_slot)).size(); ii++)
+                    (*(aCar->queue_Vehicles)).push_back( (*(pkt->hashtable_vehicles))[ii]);
 
             }else{//对于非同车道的车，若听到了一个tail，则将其车队信息更新到THN不可用中
                 if(pkt->srcVehicle->role_condition == ROLE_T){
@@ -378,7 +420,9 @@ bool IsMergingCollision(struct vehicle* aCar, int slot){
 
         if(pkt->timestamp < slot){//只看最近的一个slot即可
             break;
-        } if(pkt->condition == 0){         //condition为0的包车辆是听不到的，也解不出
+        }
+
+        if(pkt->condition == 0){         //condition为0的包车辆是听不到的，也解不出
             continue;
         }else if(pkt->condition == 1){
             //检测是否全部认可了自己
@@ -439,283 +483,3 @@ double safe_range(struct vehicle* aCar, struct vehicle* frontV){
         return ((aCar->speed)*(aCar->speed) - (frontV->speed)*(frontV->speed))/2/aCar->acc +20;
     }
 }
-
-
-
-
-////determine the communication range
-//void bubble_protocol_commRange(struct Duallist *ALL_Vehicles, int slot){
-//    struct Item * aItem;
-//    struct vehicle *aCar;
-//
-//    aItem = ALL_Vehicles->head;
-//    while(aItem != NULL) {
-//        aCar = (struct vehicle*)aItem->datap;
-//
-//        if(aCar->frontV == nullptr && aCar->rearV == nullptr){
-//            aCar->commRadius = safe_range(aCar, aCar->frontV);
-//        }else if(aCar->frontV == nullptr && aCar->rearV != nullptr){
-//            aCar->commRadius = max(safe_range(aCar, aCar->frontV), distance_between_vehicle(aCar, aCar->rearV));
-//        }else if(aCar->frontV != nullptr && aCar->rearV == nullptr){
-//            aCar->commRadius = max(safe_range(aCar, aCar->frontV), distance_between_vehicle(aCar, aCar->frontV));
-//        }else{
-//            aCar->commRadius = max(safe_range(aCar, aCar->frontV),max(distance_between_vehicle(aCar, aCar->frontV), distance_between_vehicle(aCar, aCar->rearV)));
-//        }
-//        aItem = aItem->next;
-//    }
-//}
-//// 添加一下处理听到的一整个车队的时槽使用情况
-//// 所有车辆处理其收到的一帧之内的packets
-//void bubble_handle_packets(struct Duallist *ALL_Vehicles, int slot){
-//    struct Item * aItem, *bItem;
-//    struct vehicle *aCar;
-//
-//    aItem = ALL_Vehicles->head;
-//    while(aItem != NULL){
-//        aCar = (struct vehicle*)aItem->datap;
-//
-//        //初始化记录的变量，也就是更新这些东西啦
-//        aCar->front_Vehicles = new vector<struct vehicle*>;
-//        aCar->rearV_Vehicles = new vector<struct vehicle*>;
-//        aCar->queue_Vehicles = new vector<struct vehicle*>;
-//        aCar->queue_Vehicles_slot = new vector<int>;
-//        aCar->frontV = nullptr;
-//        aCar->rearV = nullptr;
-//
-//        for(int i = 0; i < SlotPerFrame; i++){
-//            aCar->OHN[i] = nullptr;
-//            aCar->THN[i] = nullptr;
-//        }
-//
-//        bItem = (struct Item*)aCar->packets.head;
-//        while(bItem != NULL){
-//            struct packet* pkt = (struct packet*)bItem->datap;
-//            if(pkt->condition == TX_COLI){//这个包属于两个同时发射，无法被感知到
-//                bItem = bItem->next;
-//            }else if(pkt->condition == NO_COLI){//这种属于能够正常解的包
-//                //更新OHN
-//                int index = (pkt->timestamp)%SlotPerFrame;
-//                aCar->OHN[index] = pkt->srcVehicle;
-//
-//                //更新THN
-//                for(int i = 0; i < SlotPerFrame; i++){
-//                    if(pkt->OHN_snapshot[i]!=NULL)
-//                        aCar->THN[i] = pkt->OHN_snapshot[i];
-//                }
-//                //更新front_Vehicles, rear_Vehicles
-//                if(strcmp(aCar->lane, pkt->srcVehicle->lane) == 0){ //处于相同车道
-//                    if(pkt->srcVehicle->pos > aCar->pos){//同车道前方
-//                        (*(aCar->front_Vehicles)).push_back(pkt->srcVehicle);
-//                    }else{//同车道后方
-//                        (*(aCar->rearV_Vehicles)).push_back(pkt->srcVehicle);
-//                    }
-//
-//                    // 更新同车道前后最近邻车辆的信息
-//                    aCar->forntV_his = aCar->frontV;
-//                    aCar->rearV_his = aCar->rearV_his;
-//                    aCar->rearV = nearestVehicle(aCar, *(aCar->rearV_Vehicles));
-//                    aCar->frontV = nearestVehicle(aCar, *(aCar->front_Vehicles));
-//
-//                    // 更新车队信息,哪些车用了什么时槽
-////                    for(auto tmp: pkt->hashtable){
-////                        struct vehicle* bCar = tmp.first;
-////                        int slot_index = tmp.second;
-////                        aCar->queue_Vehicles[bCar] = slot_index;
-////                    }
-//                    for(int ii = 0; ii < (*(pkt->hashtable_slot)).size(); ii++){
-//                        struct vehicle* bCar = (*(pkt->hashtable_vehicles))[ii];
-//                        int slot_index = (*(pkt->hashtable_slot))[ii];
-//
-//                        (*(aCar->queue_Vehicles)).push_back(bCar);
-//                        (*(aCar->queue_Vehicles_slot)).push_back(slot_index);
-//                    }
-//
-//                }else{//对于非同车道的车，若听到了一个tail，则将其车队信息更新到THN不可用中
-//                    if(pkt->srcVehicle->role_condition == ROLE_T){
-////                        for(auto tmp: pkt->hashtable){
-////                            struct vehicle* bCar = tmp.first;
-////                            int slot_index = tmp.second;
-////                            aCar->THN[slot_index] = bCar;
-////                        }
-//                        for(int ii = 0; ii < (*(pkt->hashtable_slot)).size(); ii++){
-//                            struct vehicle* bCar = (*(pkt->hashtable_vehicles))[ii];
-//                            int slot_index = (*(pkt->hashtable_slot))[ii];
-//
-//                            (*(aCar->queue_Vehicles)).push_back(bCar);
-//                            (*(aCar->queue_Vehicles_slot)).push_back(slot_index);
-//                        }
-//                    }
-//                }
-//                bItem = bItem->next;
-//            }else if(pkt->condition == RX_COLI){//这种属于1个接收端同时有多个包送达，不能解出包，但是能感知到包的存在
-//                int index = (pkt->timestamp)%SlotPerFrame;
-//                aCar->OHN[index] = pkt->srcVehicle;
-//                bItem = bItem->next;
-//            }
-//        }
-//        delete aCar->front_Vehicles;
-//        delete aCar->rearV_Vehicles;
-//        delete aCar->queue_Vehicles;
-//        delete aCar->queue_Vehicles_slot;
-//
-//        aItem = aItem->next;
-//    }
-//    //cout<<"hello!!!"<<endl;
-//}
-//
-////determine the slot
-//void bubble_protocol_slot(struct Duallist *ALL_Vehicles, int slot){
-//    struct Item * aItem;
-//    struct vehicle *aCar;
-//
-//    aItem = ALL_Vehicles->head;
-//    while(aItem != NULL) {
-//        aCar = (struct vehicle*)aItem->datap;
-//
-//        //单车的case,单车的时候，随机选择
-//        if(aCar->role_condition == ROLE_S) {
-//            slot_determine_for_access(aCar, ROLE_S);
-//        }else if(aCar->role_condition == ROLE_H){
-//            if(aCar->slot_condition == ACCESS){
-//                if(aCar->frontV == nullptr && aCar->rearV != nullptr && aCar->rearV->OHN[aCar->slot_occupied] == aCar){
-//                    aCar->slot_condition = OCCUPIED;//被后车认可
-//                    aCar->counter = 0;
-//                }else{
-//                    slot_determine_for_access(aCar, ROLE_H);
-//                }
-//            }else if(aCar->slot_condition == OCCUPIED){
-//                if(aCar->frontV == nullptr && aCar->rearV != nullptr && aCar->rearV->OHN[aCar->slot_occupied] == aCar){
-//                    aCar->slot_condition = OCCUPIED;//被后车认可
-//                    aCar->counter = 0;
-//                }else{
-//                    aCar->counter ++;
-//                }
-//                if(aCar->counter == counterToChange){
-//                    slot_determine_for_access(aCar, ROLE_H);
-//                }
-//            }else{
-//                cout<<"There is an error for slot deter"<<endl;
-//            }
-//        }else if(aCar->role_condition == ROLE_T){
-//            if(aCar->role_condition == ACCESS){
-//                if(aCar->rearV == nullptr && aCar->frontV != nullptr && aCar->frontV->OHN[aCar->slot_occupied] == aCar){
-//                    aCar->slot_condition = OCCUPIED;//被后车认可
-//                    aCar->counter = 0;
-//                }else{
-//                    slot_determine_for_access(aCar, ROLE_H);
-//                }
-//            }else if(aCar->slot_condition == OCCUPIED){
-//                if(aCar->rearV == nullptr && aCar->frontV != nullptr && aCar->frontV->OHN[aCar->slot_occupied] == aCar){
-//                    aCar->slot_condition = OCCUPIED;//被后车认可
-//                    aCar->counter = 0;
-//                }else{
-//                    aCar->counter++;
-//                }
-//
-//                if(aCar->counter == counterToChange){
-//                    slot_determine_for_access(aCar, ROLE_H);
-//                }
-//            }
-//        }else if(aCar->role_condition == ROLE_I){
-//            if(aCar->role_condition == ACCESS){
-//                if(aCar->rearV != nullptr && aCar->frontV != nullptr && aCar->frontV->OHN[aCar->slot_occupied] == aCar && aCar->rearV->OHN[aCar->slot_occupied] == aCar){
-//                    aCar->slot_condition = OCCUPIED;//被后车认可
-//                    aCar->counter = 0;
-//                }else{
-//                    slot_determine_for_access(aCar, ROLE_I);
-//                }
-//            }else if(aCar->role_condition == OCCUPIED){
-//                if(aCar->rearV != nullptr && aCar->frontV != nullptr && aCar->frontV->OHN[aCar->slot_occupied] == aCar && aCar->rearV->OHN[aCar->slot_occupied] == aCar){
-//                    aCar->slot_condition = OCCUPIED;//被前后车认可
-//                    aCar->counter = 0;
-//                }else{
-//                    aCar->counter++;
-//                }
-//                if(aCar->counter == counterToChange){
-//                    slot_determine_for_access(aCar, ROLE_H);
-//                }
-//            }
-//        }
-//        aItem = aItem->next;
-//    }
-//}
-
-//
-//void slot_determine_for_access(struct vehicle* aCar, int role){
-//
-//    if(aCar->frontV == nullptr && aCar->rearV == nullptr){
-//        aCar->role_condition = ROLE_S;
-//        aCar->slot_occupied = choose_slot(aCar, ROLE_S);
-//        aCar->slot_condition = ACCESS;
-//    }else if(aCar->frontV == nullptr && aCar->rearV != nullptr){
-//        aCar->role_condition = ROLE_H;
-//        aCar->slot_occupied = choose_slot(aCar, ROLE_H);
-//        aCar->slot_condition = ACCESS;
-//    }else if(aCar->frontV != nullptr && aCar->rearV == nullptr){
-//        aCar->role_condition = ROLE_T;
-//        aCar->slot_occupied = choose_slot(aCar, ROLE_T);
-//        aCar->slot_condition = ACCESS;
-//    }else if(aCar->frontV != nullptr && aCar->rearV != nullptr){
-//        aCar->role_condition = ROLE_I;
-//        aCar->slot_occupied = choose_slot(aCar, ROLE_I);
-//        aCar->slot_condition = ACCESS;
-//    }else{
-//        cout<<"ERROR to determine a slot for "<< role <<" vehicle"<<endl;
-//    }
-//}
-
-
-
-
-////根据角色，在对应的时槽资源块内随机进行时槽选择；拒绝采样
-//int choose_slot(struct vehicle* aCar, int role){
-//    int a, b;
-//    int ans = 0;
-//
-//        switch(role) {
-//            case ROLE_H:
-//                a = 0;
-//                b = len_head_resource -1;
-//                while(1){
-//                    ans = rand()%(b - a + 1) + a;
-//                    if(aCar->THN[ans]== NULL) break;
-//                }
-//                break;
-//            case ROLE_S:
-//                a = len_head_resource;
-//                b = (SlotPerFrame - 1)/2 - len_tail_resouce ;
-//                while(1){
-//                    ans = rand()%(b - a + 1) + a;
-//                    if(aCar->THN[ans]== NULL) break;
-//                }
-//                break;
-//            case ROLE_I:
-//                a = len_head_resource;
-//                b = (SlotPerFrame - 1)/2 - len_tail_resouce ;
-//                while(1){
-//                    ans = rand()%(b - a + 1) + a;
-//                    if(aCar->THN[ans]== NULL) break;
-//                }
-//                break;
-//            case ROLE_T:
-//                a = (SlotPerFrame - 1)/2 - len_tail_resouce;
-//                b = (SlotPerFrame - 1)/2 ;
-//                while(1){
-//                    ans = rand()%(b - a + 1) + a;
-//                    if(aCar->THN[ans]== NULL) break;
-//                }
-//                break;
-//            case ROLE_VeMAC:
-//                a = 0;
-//                b = (SlotPerFrame - 1)/2;
-//                while(1){
-//                    ans = rand()%(b - a + 1) + a;
-//                    if(aCar->THN[ans]== NULL) break;
-//                }
-//                break;
-//        }
-//        if(aCar->resource_pool == 0)
-//            return ans;
-//        else
-//            return ans + (SlotPerFrame - 1) / 2;
-//}
--- main.cpp
+++ main.cpp
@@ -12,8 +12,6 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    cout<<"hello world"<<endl;
-
 
     // Set the trace path and traffic density
     string trace_path;
@@ -21,8 +19,6 @@ int main(int argc, char *argv[]) {
 //    trace_path = argv[1];       //读取文件路径
     trace_path = "C:\\Users\\cyx02\\Desktop\\transformed\\transformed\\carposition_";
     traffic_density = 240;
-
-
 
     cout<< "Current trace file is from:"<< trace_path <<"..."<<endl;
     cout<<"Current Protocol is Bubble MAC..."<<endl;
@@ -44,18 +40,6 @@ int main(int argc, char *argv[]) {
     if(!log_statistic_file){cerr <<"create file error \n"; return 1;}
 
 
-    // Reset the Statistical Para
-    cnt_cars = 0;
-    cnt_tx_collision = 0;       //两个发射碰撞
-    cnt_rx_normal = 0;          //正常收包
-    cnt_rx_colli = 0;           //产生碰撞的包
-    cnt_pkt_tx = 0;             //发射的包个数
-    cnt_pkt_tx_normal = 0;      //发射过程中没有TX碰撞的个数
-    cnt_frontV_normal = 0;
-    cnt_rearV_normal = 0;
-    cnt_frontV_colli = 0;
-    cnt_rearV_Colli = 0;
-
     srand(0);
 
     int slot_start = 0; //每个slot是0.5毫秒
@@ -68,18 +52,15 @@ int main(int argc, char *argv[]) {
     log_process_file << "The simulation is starting..."<<endl;
     log_process_file <<"slot start from "<< slot_start<<" to "<<slot_end<<", the slot step is "<<slot_step<<endl<<endl;
 
-    Car_Number = 0;
-
-
+    // Set a collision flag. All collisions point to this address
     collision_vehicle = new struct vehicle;
     strcpy(collision_vehicle->id,"rx_collision");
 
 
-
+    // Start Simulation
     for(int slot = slot_start; slot < slot_end; slot += slot_step){
-//        if(slot %1000 == 0)
-//            cout<<"slot = "<< slot<<endl;
-
+        if(slot %SlotPerFrame == 0)
+            cout<<"Frame = "<< slot/SlotPerFrame<<endl;
 
          //Update Location every UpLocSlot slot
         if(slot % UpLocSlot == 0){
